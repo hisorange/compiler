91078b186178092e4aa4fb83529f3311d31005f7
hisorange <hello@hisorange.me>
2020-10-10 21:31:57 +0200

    Add missing exports for CLI calls

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from './generators/artgen/component/component.generator';
 export * from './generators/nestjs/crud/nestjs-crud.generator';
 // Plugins, grammars for compiling.
 export * from './grammars/aml/aml.plugin';
+export * from './grammars/wsn/wsn.plugin';
 export * from './interfaces/components/logger.interface';
 export * from './plugins/build-log/build-log.plugin';
