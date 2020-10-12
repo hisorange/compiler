3d46d6dc34d76940d31d8cb8afdc24b80e0a83df
hisorange <hello@hisorange.me>
2020-10-12 18:56:15 +0200

    Move to backends directory, only temporary

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 export * from './artgen';
+export * from './backends/artgen/template/template.backend';
+export * from './backends/nestjs/crud/nestjs-crud.backend';
 export * from './exceptions';
-export * from './generators/artgen/template/template.backend';
-export * from './generators/nestjs/crud/nestjs-crud.backend';
 // Plugins, grammars for compiling.
 export * from './grammars/aml/aml.plugin';
 export * from './grammars/wsn/wsn.plugin';
 export * from './interfaces/components/logger.interface';
 export * from './plugins/build-log/build-log.plugin';
+
