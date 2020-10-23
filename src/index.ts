edac0af87631f330ea7ec51fcb8c8b1ff31a0314
hisorange <hello@hisorange.me>
2020-10-23 14:14:33 +0200

    Exporting more and more for plugins

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 export * from './artgen';
+export * from './backends/artgen/backend/backend.backend';
+export * from './backends/artgen/grammar/grammar.backend';
 export * from './backends/artgen/template/template.backend';
 export * from './backends/nestjs/crud/nestjs-crud.backend';
+export * from './decorators/template.decorator';
 export * from './exceptions';
 // Plugins, grammars for compiling.
 export * from './grammars/aml/aml.plugin';
 export * from './grammars/wsn/wsn.plugin';
 export * from './interfaces/components/logger.interface';
+export * from './interfaces/template.interface';
 export * from './plugins/build-log/build-log.plugin';
-
