58028a052bb687b423c961ded183d905f6fbfdf4
Varga Zsolt <hello@hisorange.me>
2021-07-04 19:58:48 +0200

    Simplify the parser registration

--- src/builtins/frontends/agl/agl.parser.ts
+++ src/builtins/frontends/agl/agl.parser.ts
@@ -25,55 +25,55 @@ export class GrammarParser extends BaseParser {
 
   registerParsers(): void {
     // Struct: WS
-    this.registerParser(['WS.EOL'], this.createParser_WS_EOL());
-    this.registerParser(['WS.Tab'], this.createParser_WS_Tab());
-    this.registerParser(['WS.Space'], this.createParser_WS_Space());
-    this.registerParser(['WS.Set'], this.createParser_WS_Set());
-    this.registerParser(['WS.Any', 'WS'], this.createParser_WS_Any());
-
-    // Struct: ---
-    this.registerParser(['Letter'], this.createParser_Letter());
-    this.registerParser(['Digit'], this.createParser_Digit());
-    this.registerParser(['Float'], this.createParser_Float());
-    this.registerParser(['Number'], this.createParser_Number());
-    this.registerParser(['Symbol'], this.createParser_Symbol());
+    this.createStruct_WS();
   }
 
-  createParser_WS_EOL() {
-    const ref = 'WS.EOL';
+  protected createStruct_WS() {
     const channel = 'WhiteSpace';
 
+    this.registerParser(
+      ['WS.EOL'],
+      this.createParser_WS_EOL('WS.EOL', channel),
+    );
+
+    this.registerParser(
+      ['WS.Tab'],
+      this.createParser_WS_Tab('WS.Tab', channel),
+    );
+
+    this.registerParser(
+      ['WS.Space'],
+      this.createParser_WS_Space('WS.Space', channel),
+    );
+
+    this.registerParser(
+      ['WS.Set'],
+      this.createParser_WS_Set('WS.Set', channel),
+    );
+
+    this.registerParser(
+      ['WS.Any', 'WS'],
+      this.createParser_WS_Any('WS.Any', channel),
+    );
+  }
+
+  createParser_WS_EOL(ref: string, channel: string) {
     const orGroup = [];
     orGroup.push(this.parseLiteral(ref, channel, `\n`));
     orGroup.push(this.parseLiteral(ref, channel, `\r\n`));
 
-    const parser = this.parseLogicOr(orGroup);
-
-    return parser;
+    return this.parseLogicOr(orGroup);
   }
 
-  createParser_WS_Tab() {
-    const ref = 'WS.Tab';
-    const channel = 'WhiteSpace';
-
-    const parser = this.parseLiteral(ref, channel, `\t`);
-
-    return parser;
+  createParser_WS_Tab(ref: string, channel: string) {
+    return this.parseLiteral(ref, channel, `\t`);
   }
 
-  createParser_WS_Space() {
-    const ref = 'WS.Space';
-    const channel = 'WhiteSpace';
-
-    const parser = this.parseLiteral(ref, channel, ` `);
-
-    return parser;
+  createParser_WS_Space(ref: string, channel: string) {
+    return this.parseLiteral(ref, channel, ` `);
   }
 
-  createParser_WS_Set() {
-    const ref = 'WS.Set';
-    const channel = 'WhiteSpace';
-
+  createParser_WS_Set(ref: string, channel: string) {
     const orGroup = [];
     orGroup.push(this.nestParser('WS.EOL'));
     orGroup.push(this.nestParser('WS.Space'));
@@ -85,38 +85,23 @@ export class GrammarParser extends BaseParser {
     return parser;
   }
 
-  createParser_WS_Any() {
-    const ref = 'WS.Any';
-    const channel = 'WhiteSpace';
-
-    const term_1 = this.nestParser('WS.Set');
-    const parser = this.parseRepetitionNoneOrMore(ref, channel, term_1);
-
-    return parser;
+  createParser_WS_Any(ref: string, channel: string) {
+    return this.parseRepetitionNoneOrMore(
+      ref,
+      channel,
+      this.nestParser('WS.Set'),
+    );
   }
 
-  createParser_Letter() {
-    const ref = 'Letter';
-    const channel = 'Main';
-
-    const parser = this.parseRange(ref, channel, 'a', 'Z');
-
-    return parser;
+  createParser_Letter(ref: string, channel: string) {
+    return this.parseRange(ref, channel, 'a', 'Z');
   }
 
-  createParser_Digit() {
-    const ref = 'Digit';
-    const channel = 'Main';
-
-    const parser = this.parseRange(ref, channel, '0', '9');
-
-    return parser;
+  createParser_Digit(ref: string, channel: string) {
+    return this.parseRange(ref, channel, '0', '9');
   }
 
-  createParser_Float() {
-    const ref = 'Float';
-    const channel = 'Main';
-
+  createParser_Float(ref: string, channel: string) {
     const andGroup = [];
 
     andGroup.push(
@@ -129,30 +114,21 @@ export class GrammarParser extends BaseParser {
       this.parseRepetitionOneOrMore(ref, channel, this.resolveParser('Digit')),
     );
 
-    const parser = this.parseLogicAnd(andGroup);
-
-    return parser;
+    return this.parseLogicAnd(andGroup);
   }
 
-  createParser_Number() {
-    const ref = 'Number';
-    const channel = 'Main';
-
+  createParser_Number(ref: string, channel: string) {
     const orGroup = [];
 
     orGroup.push(this.resolveParser('Digit'));
     orGroup.push(this.resolveParser('Float'));
 
     const orParser = this.parseLogicOr(orGroup);
-    const parser = this.wrapParser(ref, channel, orParser);
 
-    return parser;
+    return this.wrapParser(ref, channel, orParser);
   }
 
-  createParser_Symbol() {
-    const ref = 'Symbol';
-    const channel = 'Main';
-
+  createParser_Symbol(ref: string, channel: string) {
     const orGroup = [];
 
     orGroup.push(this.resolveParser('Digit'));
@@ -160,15 +136,11 @@ export class GrammarParser extends BaseParser {
     orGroup.push(this.resolveParser('WS'));
 
     const orParser = this.parseLogicOr(orGroup);
-    const parser = this.invertParser(ref, channel, orParser);
 
-    return parser;
+    return this.invertParser(ref, channel, orParser);
   }
 
-  createParser_NumericRange() {
-    const ref = 'NumericRange';
-    const channel = 'Main';
-
+  createParser_NumericRange(ref: string, channel: string) {
     const andGroup = [];
 
     andGroup.push(
@@ -188,15 +160,11 @@ export class GrammarParser extends BaseParser {
     );
 
     const andParser = this.parseLogicAnd(andGroup);
-    const parser = this.wrapParser(ref, channel, andParser);
 
-    return parser;
+    return this.wrapParser(ref, channel, andParser);
   }
 
-  createParser_AlphabeticRange() {
-    const ref = 'AlphabeticRange';
-    const channel = 'Main';
-
+  createParser_AlphabeticRange(ref: string, channel: string) {
     const andGroup = [];
 
     andGroup.push(this.resolveParser('Letter'));
@@ -204,37 +172,27 @@ export class GrammarParser extends BaseParser {
     andGroup.push(this.resolveParser('Letter'));
 
     const andParser = this.parseLogicAnd(andGroup);
-    const parser = this.wrapParser(ref, channel, andParser);
 
-    return parser;
+    return this.wrapParser(ref, channel, andParser);
   }
 
-  createParser_Boolean() {
-    const ref = 'Boolean';
-    const channel = 'Main';
-
+  createParser_Boolean(ref: string, channel: string) {
     const orGroup = [];
 
     orGroup.push(this.parseLiteral(ref, channel, `true`));
     orGroup.push(this.parseLiteral(ref, channel, `false`));
 
-    const parser = this.parseLogicOr(orGroup);
-
-    return parser;
+    return this.parseLogicOr(orGroup);
   }
 
-  createParser_Range() {
-    const ref = 'Range';
-    const channel = 'Main';
-
+  createParser_Range(ref: string, channel: string) {
     const orGroup = [];
 
     orGroup.push(this.resolveParser('NumericRange'));
     orGroup.push(this.resolveParser('AlphabeticRange'));
 
     const orParser = this.parseLogicOr(orGroup);
-    const parser = this.wrapParser(ref, channel, orParser);
 
-    return parser;
+    return this.wrapParser(ref, channel, orParser);
   }
 }
